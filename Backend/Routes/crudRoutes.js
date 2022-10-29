e34ef2ceea85953169a86fd31a0dc124027ff4a7
Matthew Bunce <mbunce@uoguelph.ca>
2022-10-29 11:55:59 -0400

    Crud tested w postman

---
+++ Backend/Routes/crudRoutes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router()
+
+//CREATE
+router.post('/', (req, res) => {
+    res.status(200).json({message: 'Create Response'})
+})
+
+//READ
+router.get('/', (req, res) => {
+    res.status(200).json({message: 'Get Response'})
+})
+
+//UPDATE
+router.put('/:id', (req, res) => {
+    res.status(200).json({message: `Update item ${req.params.id}`})
+})
+
+//DELETE
+router.delete('/:id', (req, res) => {
+    res.status(200).json({message: `Delete item ${req.params.id}`})
+})
+
+
+module.exports = router
--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const port = 5000
+const port = process.env.PORT || 8000
 
 const app = express()
+
+app.use('/api/items', require('./Routes/crudRoutes'))
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
